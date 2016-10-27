672ce0a9c6f6c1bb2e93ebbbba48b2950b5e8ccc
Yuhan Luo <ailsa4v@gmail.com>
2016-10-26 20:32:49 -0400

    add scroll bar

--- 03-census/request_data/request_data.js
+++ 03-census/request_data/request_data.js
@@ -23,7 +23,7 @@ var barColorList = {'B01003_001E': '#ffffff'
 var textColor = '#a2e0fc';
 
 var foo = 0;
-var yOffset = 0;
+var yOffset = -20;
 function setup() {
     createCanvas(960, 540);
     // initializeCensus('');
@@ -86,82 +86,70 @@ function draw() {
         //   var countyNames = Object.keys(USData);
 
         noStroke();
-        textAlign(RIGHT, TOP);
-        fill(200);
-        text("Below the poverty line", width/3-5, 2);
-
         textAlign(LEFT, TOP);
-        fill(255, 227, 61, 220);
-        text("Mean per person,", width/3+5, 2);
-        fill(255,244,230, 230);
-        text("Median household", width/3+100, 2);
-
-
         var count = 0;
-        var len = Object.keys(stateList).length;
+        var len = Object.keys(stateList).length / 3;
 
-          // if (foo = 1) {
-            // 1 bar with percentage on left and 1 abr on right, shown when a right click happens
-            for (var stateName in sortObjByFeature(stateList,'B19301_001E')) {
-              count += 1;
-              var barWidth = height/(len + 1)/3.5;
-              var y = map(count, 0, len, 10, height - 10);
+        for (var stateName in sortObjByFeature(stateList,'B19301_001E')) {
+          count += 1;
+          var barWidth = height/(len + 1)/2.5;
+          var y = map(count, 0, len, 10, height - 10) - yOffset;
 
-              (count % 2) ? fill(255) : fill("#FFE33D");
-              textSize(11);
-              text(stateName, 5, y-barWidth*1.5);
-
-              var poverty_percent = stateList[stateName]['B17001_002E']/stateList[stateName]['B01003_001E'];
-              var w = map(poverty_percent, 0, 0.5, 0, width/3-60);
-              fill(100);
-              rect(width/3-w, y, w, barWidth*2);
-              fill("#AC9475");
-              text(nf(poverty_percent,0,2)*100 + "%", width/3-w-35, y-barWidth*1.5);
-
-              var y1 = map(count, 0, len, 10, height - 10);
-              var w1 = map(stateList[stateName]['B19301_001E'], 10000, 80000, 10, width/3*2-40);
-              var w2 = map(stateList[stateName]['B19013_001E'], 10000, 80000, 10, width/3*2-40);
-              textSize(8);
-              fill(255, 227, 61, 220);
-              rect(width/3, y1, w1, barWidth);
-              text(stateList[stateName]['B19301_001E'], width/3+w1+10, y1-barWidth*2);
-              fill(255,244,230, 230);
-              rect(width/3, y1+barWidth, w2, barWidth);
-              // fill(255);
-              text(stateList[stateName]['B19013_001E'], width/3+w2+5, y1);
-            }
-          // } else {
-            // 2 bars
-            // for (var stateName in sortObjByFeature(stateList,'B01003_001E')) {
-            //   count += 1;
-            //   var barWidth = height/(len + 1)/3;
-            //   var y = map(count, 0, len, 10, height - 10);
-            //   for (var variableCode in stateList[stateName]){
-            //     var w = map(stateList[stateName][variableCode], dataMin, dataMax, 0, width/2 - 100);
-            //     fill(color(barColorList[variableCode]));
-            //     rect(width/2, y, w, barWidth);
-            //     fill(color(textColor));
-            //     if (variableCode === 'B01003_001E'){
-            //       text(stateName, width/2+w+5, y-barWidth*1.5);
-            //     }
-            //   }
-            // }
-
-          // }
-          fill(255);
-          stroke(backgroundColor);
-          rect(width/3-1.5, 0, 3, height);
-
-          fill(255, 80);
-          stroke(255, 227, 61);
-          rect(width*0.85, height*0.05, width*0.13, height*0.2);
-          fill(255);
-          noStroke();
+          (count % 2) ? fill(255) : fill("#FFE33D");
           textSize(15);
-          textAlign(CENTER, CENTER);
-          text("SORT BY", width*0.91, height*0.05 + 10);
+          text(stateName, 5, y+2);
 
-          // }
+          var poverty_percent = stateList[stateName]['B17001_002E']/stateList[stateName]['B01003_001E'];
+          var w = map(poverty_percent, 0, 0.5, 0, width/3-60);
+          fill(100);
+          rect(width/3-w, y, w, barWidth*2);
+          fill(255,200);
+          textAlign(RIGHT, TOP);
+          text(Number(poverty_percent*100).toFixed(1)+ "%", width/3-2, y);
+
+          textAlign(LEFT, TOP);
+          var y1 = map(count, 0, len, 10, height - 10) - yOffset;
+          var w1 = map(stateList[stateName]['B19301_001E'], 10000, 80000, 10, width/3*2-40);
+          var w2 = map(stateList[stateName]['B19013_001E'], 10000, 80000, 10, width/3*2-40);
+          // textSize(8);
+          fill(255, 227, 61, 220);
+          rect(width/3, y1, w1, barWidth);
+          text(stateList[stateName]['B19301_001E'], width/3+w1+10, y1);
+          fill(255,244,230, 230);
+          rect(width/3, y1+barWidth, w2, barWidth);
+          text(stateList[stateName]['B19013_001E'], width/3+w2+5, y1+barWidth);
+        }
+
+        fill(255);
+        stroke(backgroundColor);
+        rect(width/3-1.5, 0, 3, height);
+
+        fill(255, 80);
+        stroke(255, 227, 61);
+        rect(width*0.85, height*0.05, width*0.13, height*0.2);
+        fill(255);
+        noStroke();
+        textSize(15);
+        textAlign(CENTER, CENTER);
+        text("SORT BY", width*0.91, height*0.05 + 10);
+
+        fill(backgroundColor);
+        rect(0, 0, width, 45);
+
+        textAlign(CENTER, TOP);
+        fill(255);
+        textSize(20);
+        text("Rich v.s Poor", width/2, 2);
+
+        textSize(12);
+        textAlign(RIGHT, BOTTOM);
+        fill(200);
+        text("Below the poverty line", width/3-5, 40);
+        textAlign(LEFT, BOTTOM);
+        fill(255, 227, 61, 220);
+        text("Mean per person,", width/3+5, 40);
+        fill(255,244,230, 230);
+        text("Median household", width/3+100, 40);
       }
     }
 
@@ -179,8 +167,16 @@ function draw() {
 function mouseWheel(event) {
   print(event.delta);
   //move the square according to the vertical scroll amount
-  yOffset += event.delta;
   console.log(yOffset);
+  if (yOffset > height + 550) {
+    yOffset = height + 550;
+  } else if (yOffset < -20) {
+    yOffset = -20;
+  } else {
+    yOffset += event.delta;
+  }
+  //uncomment to block page scrolling
+  return false;
   //uncomment to block page scrolling
   //return false;
 }
