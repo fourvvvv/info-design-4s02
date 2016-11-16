311892c7418e043b430cedd089b0c446877c9d80
Yuhan Luo <yuhanluov@gmail.com>
2016-11-15 21:27:19 -0500

    lesser houses emerge after they first involved
    DONE 2) lesser houses emerge after they first involved
      - if house involved a battle, set "involved" as 1
      - only display house whose "involved" == 1
      - houses' #battle -> color

--- final/final.js
+++ final/final.js
@@ -1,7 +1,30 @@
+/*****
+TODO:
+1) arrow
+DONE 2) lesser houses emerge after they first involved
+  - if house involved a battle, set "involved" as 1
+  - only display house whose "involved" == 1
+  - houses' #battle -> color
+3) Bolton betrayed
+4) get icon image for each house
+*****/
+
+/***
+NOTE:
+1) current_index = slider.value() - 1
+***/
+
 // global variables
 var if_data_print = 0;
 var slider;
 var data;
+var img = [];
+
+function preload() {
+  for (var i = 0; i < houses.length; i++) {
+    if (houses[i]["great"]) houses[i]["img"] = loadImage("img/" + houses[i]["name"] +".png");
+  }
+}
 
 function setup() {
   data = loadTable("data/battles.csv", "csv", "header");
@@ -47,10 +70,12 @@ function draw() {
   // draw time slider
   drawTimeSlider();
 
+  // fill involoved
+  if (data.getRowCount()) fillInvoloved(slider.value() - 1)
   // draw info
   textAlign(LEFT);
   textSize(15);
-  drawInfo(slider.value()-1);
+  drawInfo(slider.value() - 1);
 
   // draw circle
   if (data.getRowCount()) drawCircle(slider.value()-1);
@@ -82,24 +107,36 @@ function drawInfo(index) {
 function drawCircle(index) {
   // draw all houses
   for (var i = 0; i < houses.length; i++) {
-    fill(200);
-    ellipse(houses[i]['x'], houses[i]['y'], (houses[i]['great'])?80:40);
+    // if (houses[i]["great"] || houses[i]["involved"]) {
+    //   console.log(i);
+    //   fill(100 + houses[i]["involved"]/34 * 400);
+    // }
     textAlign(CENTER, CENTER);
-    fill(51);
-    text(houses[i]['name'], houses[i]['x'], houses[i]['y']);
+    if (houses[i]["img"]) {
+      fill(100 + houses[i]["involved"]/34 * 400);
+      // ellipse(houses[i]['x'], houses[i]['y'], 80);
+      image(houses[i]["img"], houses[i]['x']-40, houses[i]['y']-40, 80, 80);
+      // fill(255);
+      text(houses[i]['name'], houses[i]['x'], houses[i]['y']+40);
+    } else {
+      noFill();
+      if (houses[i]["involved"]) {
+        stroke(100 + houses[i]["involved"]/34 * 400);
+      }
+      ellipse(houses[i]['x'], houses[i]['y'], 40);
+      if (houses[i]["involved"]) {
+        fill(100 + houses[i]["involved"]/34 * 400);
+      }
+      textSize(10);
+      noStroke();
+      text(houses[i]['name'], houses[i]['x'], houses[i]['y']);
+    }
   }
 
   // identify who's involved in the battle
-  var attackers = [];
-  var defenders = [];
-  for (var i = 5; i < 9; i++) {
-    var houseName = data.getString(index, i);
-    if (houseName) attackers.push(houseName);
-  }
-  for (var i = 9; i < 13; i++) {
-    var houseName = data.getString(index, i);
-    if (houseName) defenders.push(houseName);
-  }
+  var housesInBattle = getHousesInBattle(index);
+  var attackers = housesInBattle["attackers"];
+  var defenders = housesInBattle["defenders"];
 
   // draw arrows
   stroke(255);
@@ -113,7 +150,6 @@ function drawCircle(index) {
   }
   noStroke();
   strokeWeight(1);
-
 }
 
 // helpers
@@ -122,4 +158,45 @@ function findItemByValue(json, col, value) {
   return json.filter(
         function(json){ return json[col] == value }
     )[0];
+}
+
+function fillInvoloved(index) {
+  for (var i = 0; i < houses.length; i++) {
+    houses[i]["involved"] = 0;
+  }
+
+  for (var i = 0; i <= index; i++) {
+    var housesInBattle = getHousesInBattle(i);
+    var attackers = housesInBattle["attackers"];
+    var defenders = housesInBattle["defenders"];
+    for (var j = 0; j < attackers.length; j++) {
+      if (findItemByValue(houses, "name", attackers[j])["involved"]) {
+        findItemByValue(houses, "name", attackers[j])["involved"] += 1;
+      } else {
+        findItemByValue(houses, "name", attackers[j])["involved"] = 1;
+      }
+    }
+    for (var j = 0; j < defenders.length; j++) {
+      if (findItemByValue(houses, "name", defenders[j])["involved"]) {
+        findItemByValue(houses, "name", defenders[j])["involved"] += 1;
+      } else {
+        findItemByValue(houses, "name", defenders[j])["involved"] = 1;
+      }
+    }
+  }
+}
+
+function getHousesInBattle(index) {
+  var attackers = [];
+  var defenders = [];
+  for (var i = 5; i < 9; i++) {
+    var houseName = data.getString(index, i);
+    if (houseName) attackers.push(houseName);
+  }
+  for (var i = 9; i < 13; i++) {
+    var houseName = data.getString(index, i);
+    if (houseName) defenders.push(houseName);
+  }
+  return {"attackers": attackers
+          ,"defenders": defenders}
 }
